1f1f36fdbf8d3649760ae27e57a5153d3de1e759
Denis Surkov <149530158+Denjamins@users.noreply.github.com>
2023-11-19 14:05:21 +0400

    Update rclone-init.sh
    change ver num

--- rclone-init.sh
+++ rclone-init.sh
@@ -5,7 +5,7 @@
 # GitHub: https://github.com/Denjamins/rclone-init 
 
 # Current version of the script
-current_version="1.00"
+current_version="1.01"
 
 # Function to check the version
 check_version() {
